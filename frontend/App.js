0c14dbf815f2dce73fc2e658aac05973c151cd79
Your Name <saloni.arch@gmail.com>
2024-11-14 22:29:08 +0100

    moved navigation to AppNavigator.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,100 +1,17 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-// Import Screens
-import SplashScreenComponent from './src/screens/SplashScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import CreateEventScreen from './src/screens/CreateEventScreen'; 
-import ProfileScreen from './src/screens/ProfileScreen';
-import ChatScreen from './src/screens/ChatScreen';
-import EditProfileScreen from './src/screens/EditProfileScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import AuthScreen from './src/screens/AuthScreen';
+// Import Navigation
+import AppNavigator from './src/navigation/AppNavigator';
+import CreateEventScreen from './src/screens/CreateEventScreen';
 
 // Auth Context
 import { AuthProvider } from './src/contexts/AuthContext';
 import { UserProvider } from './src/contexts/UserContext';
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-const TabNavigator = ({ openEventModal }) => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-        if (route.name === 'Home') {
-          iconName = focused ? 'home' : 'home-outline';
-        } else if (route.name === 'Profile') {
-          iconName = focused ? 'person' : 'person-outline';
-        } else if (route.name === 'Events') {
-          iconName = focused ? 'add-circle' : 'add-circle-outline';
-        }
-        return <Icon name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#8ACE00',
-      tabBarInactiveTintColor: 'gray',
-      tabBarStyle: {
-        height: 80,
-        paddingTop: 15,
-        paddingBottom: 24,
-        backgroundColor: '#000000',
-      },
-    })}
-  >
-    <Tab.Screen 
-      name="Home" 
-      component={HomeScreen} 
-      options={({ navigation }) => ({
-        headerShown: true,
-        headerTitle: 'HOME',
-        headerTitleAlign: 'left',
-        headerTitleStyle: { fontFamily: 'Anton', fontWeight: 'bold', fontSize: 26, color: '#8ACE00' },
-        headerStyle: { backgroundColor: '#000000' },
-        headerRight: () => (
-          <Icon.Button
-            name="chatbubbles-outline"
-            size={25}
-            backgroundColor="#000000"
-            color="#8ACE00"
-            onPress={() => navigation.navigate('Chat')}
-          />
-        ),
-      })}
-    />
-    <Tab.Screen 
-      name="Events" 
-      component={HomeScreen} 
-      listeners={{
-        tabPress: (e) => {
-          e.preventDefault();
-          openEventModal(); // Open the modal when the Events tab is pressed
-        },
-      }}
-      options={{
-        headerShown: true,
-        headerTitle: 'CREATE AN EVENT',
-        headerTitleAlign: 'left',
-        headerTitleStyle: { fontFamily: 'Anton', fontWeight: 'bold', fontSize: 26, color: '#8ACE00' },
-        headerStyle: { backgroundColor: '#000000' },
-      }}
-    />
-    <Tab.Screen 
-      name="Profile" 
-      component={ProfileScreen} 
-      options={{
-        headerShown: false,
-      }}
-    />
-  </Tab.Navigator>
-);
 
 const App = () => {
   const [isModalVisible, setIsModalVisible] = useState(false); 
@@ -126,14 +43,7 @@ const App = () => {
       <UserProvider>
        <GestureHandlerRootView style={{ flex: 1}}>
         <NavigationContainer onReady={onLayoutRootView}>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Splash" component={SplashScreenComponent} />
-            <Stack.Screen name="Main" children={() => <TabNavigator openEventModal={openEventModal} />} />
-            <Stack.Screen name="Chat" component={ChatScreen} />
-            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-            <Stack.Screen name="Settings" component={SettingsScreen} />
-            <Stack.Screen name="Logout" component={AuthScreen} />
-          </Stack.Navigator>
+          <AppNavigator openEventModal={openEventModal} />
         </NavigationContainer>
         <CreateEventScreen isVisible={isModalVisible} onClose={closeEventModal} />
         </GestureHandlerRootView>
--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -1,31 +1,102 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoggedOutScreen from './LoggedOutScreen';
-import HomeScreen from './HomeScreen';
-import ProfileScreen from './ProfileScreen';
+import React from 'react';
+import colors from '../styles/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+// Import Screens
+import AuthScreen from '../screens/AuthScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import SplashScreen from '../screens/SplashScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import EditProfileScreen from '../screens/EditProfileScreen';
+import CreateEventScreen from '../screens/CreateEventScreen';
 
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen 
-      name="LoggedOut" 
-      component={LoggedOutScreen} 
-      options={{ 
-        headerShown: false, // Hide header
-        gestureEnabled: false // Disable the swipe gesture
-      }} 
-    />
-    <Stack.Screen 
-      name="Main" 
+const TabNavigator = ({ openEventModal }) => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Home') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Profile') {
+          iconName = focused ? 'person' : 'person-outline';
+        } else if (route.name === 'Events') {
+          iconName = focused ? 'add-circle' : 'add-circle-outline';
+        }
+        return <Icon name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: colors.primary,
+      tabBarInactiveTintColor: 'gray',
+      tabBarStyle: {
+        height: 80,
+        paddingTop: 15,
+        paddingBottom: 24,
+        backgroundColor: colors.black,
+      },
+    })}
+  >
+    <Tab.Screen 
+      name="Home" 
       component={HomeScreen} 
-      options={{ headerShown: false }} 
+      options={({ navigation }) => ({
+        headerShown: true,
+        headerTitle: 'HOME',
+        headerTitleAlign: 'left',
+        headerTitleStyle: { fontFamily: 'Anton', fontWeight: 'bold', fontSize: 26, color: colors.primary },
+        headerStyle: { backgroundColor: colors.black },
+        headerRight: () => (
+          <Icon.Button
+            name="chatbubbles-outline"
+            size={25}
+            backgroundColor="#000000"
+            color="#8ACE00"
+            onPress={() => navigation.navigate('Chat')}
+          />
+        ),
+      })}
     />
-    <Stack.Screen 
+    <Tab.Screen 
+      name="Events" 
+      component={HomeScreen} 
+      listeners={{
+        tabPress: (e) => {
+          e.preventDefault();
+          openEventModal(); // Open the modal when the Events tab is pressed
+        },
+      }}
+      options={{
+        headerShown: true,
+        headerTitle: 'CREATE AN EVENT',
+        headerTitleAlign: 'left',
+        headerTitleStyle: { fontFamily: 'Anton', fontWeight: 'bold', fontSize: 26, color: colors.primary },
+        headerStyle: { backgroundColor: colors.black },
+      }}
+    />
+    <Tab.Screen 
       name="Profile" 
       component={ProfileScreen} 
+      options={{
+        headerShown: false,
+      }}
     />
-  </Stack.Navigator>  
+  </Tab.Navigator>
+);
+
+const AppNavigator = ({ openEventModal }) => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Splash" component={SplashScreen} />
+    <Stack.Screen name="Main" children={() => <TabNavigator openEventModal={openEventModal} />} />
+    <Stack.Screen name="Chat" component={ChatScreen} />
+    <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+    <Stack.Screen name="Settings" component={SettingsScreen} />
+    <Stack.Screen name="Logout" component={AuthScreen} />
+  </Stack.Navigator>
 );
 
 export default AppNavigator;
